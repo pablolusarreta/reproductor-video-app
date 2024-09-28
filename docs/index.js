7bcea124d9cc1297f3f8d2792aa13d19a562aff1
Pablo Lusarreta <pablo.lusarreta@gmail.com>
2024-09-28 08:58:04 +0200

    Obtimizado

--- docs/index.js
+++ docs/index.js
@@ -1,11 +1,14 @@
 const videoInput = document.getElementById('fichero');
 const videoPlayer = document.getElementById('reproductor');
 const pie = document.getElementById('pie');
+const boton = document.getElementById('abrir');
 
 videoInput.addEventListener('change', function () {
+    document.documentElement.requestFullscreen()
     const file = this.files[0];
     if (file) {
         videoInput.style.display = "none"
+        boton.style.display = "none"
         pie.style.display = "none"
         
         const videoURL = URL.createObjectURL(file);
@@ -16,4 +19,9 @@ videoInput.addEventListener('change', function () {
             location.reload()
         }
     }
+});
+
+
+boton.addEventListener('click', function () {
+    videoInput.change()
 });
